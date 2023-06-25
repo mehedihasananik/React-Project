da72805408eadc8682d28533b5e22536b21d6344
marshallanique <marshallanique@gmail.com>
2023-06-25 15:07:33 +0600

    user change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/06-forms/starter/02-user-challenge.jsx";
+import Starter from "./tutorial/06-forms/starter/03-multiple-inputs";
 import Final from "./tutorial/01-useState/final/03-useState-array";
 
 function App() {

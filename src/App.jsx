5bc7b2d5973309e9a09ffe54e85a3e150608a7c1
marshallanique <marshallanique@gmail.com>
2023-06-25 15:45:56 +0600

    OtherInputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/06-forms/starter/03-multiple-inputs";
+import Starter from "./tutorial/06-forms/starter/04-other-inputs";
 import Final from "./tutorial/01-useState/final/03-useState-array";
 
 function App() {

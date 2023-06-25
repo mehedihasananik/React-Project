ccc4cd98326b37c27f73e476ff95be36dea3fe80
marshallanique <marshallanique@gmail.com>
2023-06-25 15:18:55 +0600

    multiple inputs

--- src/tutorial/06-forms/starter/03-multiple-inputs.jsx
+++ src/tutorial/06-forms/starter/03-multiple-inputs.jsx
@@ -1,28 +1,66 @@
+import { useState } from "react";
+
 const MultipleInputs = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onHandleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+    if (!user.name) return;
+    console.log(user);
+  };
+
   return (
     <div>
-      <form className="form">
+      <form onSubmit={onHandleSubmit} className="form">
         <h4>Multiple Inputs</h4>
         {/* name */}
         <div className="form-row">
           <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type="text" className="form-input" id="name" />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={user.name}
+            name="name"
+            onChange={onHandleChange}
+          />
         </div>
         {/* email */}
         <div className="form-row">
           <label htmlFor="email" className="form-label">
             Email
           </label>
-          <input type="email" className="form-input" id="email" />
+          <input
+            type="email"
+            className="form-input"
+            id="email"
+            value={user.email}
+            name="email"
+            onChange={onHandleChange}
+          />
         </div>
         {/* email */}
         <div className="form-row">
           <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type="password" className="form-input" id="password" />
+          <input
+            type="password"
+            className="form-input"
+            id="password"
+            value={user.password}
+            name="password"
+            onChange={onHandleChange}
+          />
         </div>
 
         <button type="submit" className="btn btn-block">

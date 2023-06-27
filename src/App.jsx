794dfe2459bdd0c889069468731823e4fd13958a
marshallanique <marshallanique@gmail.com>
2023-06-27 15:06:20 +0600

    use reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/09-context-api/starter/Navbar";
+import Starter from "./tutorial/10-useReducer/starter/01-useReducer";
 import Final from "./tutorial/09-context-api/final/context/Navbar";
 
 function App() {
--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 const ReducerBasics = () => {
   const [people, setPeople] = React.useState(data);
 
@@ -7,24 +7,35 @@ const ReducerBasics = () => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
   };
+  const resetFunc = () => {
+    setPeople(data);
+  };
   return (
-    <div>
+    <div className="text-center pt-5">
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+      {people.length < 1 ? (
+        <>
+          <button onClick={resetFunc}>Reset</button>
+        </>
+      ) : (
+        <>
+          <button
+            className="btn"
+            style={{ marginTop: "2rem" }}
+            onClick={() => setPeople([])}
+          >
+            clear items
+          </button>
+        </>
+      )}
     </div>
   );
 };

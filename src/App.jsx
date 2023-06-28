12e8102c70c49593aa7c665cbd5ece95d422ffbe
marshallanique <marshallanique@gmail.com>
2023-06-28 18:42:32 +0600

    lower state Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from "./tutorial/11-performance/starter/02-lower-state-challenge";
-import Final from "./tutorial/11-performance/final/02-lower-state-challenge";
+import Starter from "./tutorial/11-performance/starter/03-hooks";
+import Final from "./tutorial/11-performance/final/01-lower-state";
 
 function App() {
   return (

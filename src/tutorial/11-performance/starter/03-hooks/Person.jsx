51aae4a3d118bf85d6364b15217c85f5b87e288a
marshallanique <marshallanique@gmail.com>
2023-06-29 10:09:19 +0600

    useCallback

--- src/tutorial/11-performance/starter/03-hooks/Person.jsx
+++ src/tutorial/11-performance/starter/03-hooks/Person.jsx
@@ -1,7 +1,8 @@
-const Person = ({ name }) => {
+const Person = ({ name, removeItem, id }) => {
   return (
     <div>
       <h4>{name}</h4>
+      <button onClick={() => removeItem(id)}>Remove</button>
     </div>
   );
 };
